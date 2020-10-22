e1321d8a4abe15b0260c4a1150c221a529271eba
Ruslan Vasilenko <vasilenko.ruslan0321@gmail.com>
2020-10-22 23:30:06 +0300

    add task3,task4,task5

---
+++ task3.php
@@ -0,0 +1,11 @@
+<?php require "header.php";?>
+<section class="tasks">
+    <div class="container">
+        <div class="row justify-content-center">
+            <h2>Задание №3</h2>
+            <h3>Выучить базовые команды Linux(терминала)</h3>
+            <h3>___________________________________________</h3>
+        </div>
+    </div>
+</section>
+<?php require "footer.php";?>
---
+++ task5.php
@@ -0,0 +1,12 @@
+<?php require "header.php";?>
+    <section class="tasks">
+        <div class="container">
+            <div class="row justify-content-center">
+                <h2>Задание №5</h2>
+                <h3>Настроить LAMP на локальной машине, запустив внутренний Баш-скрипт для создания сайта</h3>
+                <h3>Создал Баш-скрипт из команд установки LAMP(из сайта faq.testsite)</h3>
+                <h1><a href="bash-script/lampbashvrv" download>Скачать файл</a></h1>
+            </div>
+        </div>
+    </section>
+<?php require "footer.php";?>
