eabb68f3495a7e096d07430070b217b595857723
Ruslan Vasilenko <vasilenko.ruslan0321@gmail.com>
2020-10-22 21:23:08 +0300

    add task2

---
+++ task2/task2.php
@@ -0,0 +1,12 @@
+<?php require "../header.php";?>
+<section class="tasks">
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1>Задание №2</h1>
+            <h3>Создать поддомен на testsite</h3>
+            <hr>
+            <h3>Создал поддомен с адресом Хоста vrvtask.testsite.co.ua</h3>
+        </div>
+    </div>
+</section>
+<?php require "../footer.php";?>
