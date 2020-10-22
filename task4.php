e78e224cdae0d33503836de27348380d46cdf277
Ruslan Vasilenko <vasilenko.ruslan0321@gmail.com>
2020-10-22 23:30:06 +0300

    add task3,task4,task5

---
+++ task4.php
@@ -0,0 +1,12 @@
+<?php require "header.php";?>
+    <section class="tasks">
+        <div class="container">
+            <div class="row justify-content-center">
+                <h2>Задание №4</h2>
+                <h3>Подключится к тестовому серверу по FTP и залить картинки</h3>
+                <h3>Подключился через быстрое соеденение по FTP через FILEZILLA</h3>
+                <h4>Картинки добавил и подключил, но они удалились после Deploy</h4>
+            </div>
+        </div>
+    </section>
+<?php require "footer.php";?>
