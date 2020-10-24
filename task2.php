58962e2f607d635295f317abb984d655b39f6759
Ruslan Vasilenko <vasilenko.ruslan0321@gmail.com>
2020-10-24 22:48:18 +0300

    add task6

--- task2.php
+++ task2.php
@@ -1,10 +1,14 @@
 <?php require "header.php";?>
-<section class="tasks">
-    <div class="container">
+<section class="tasks bg-info">
+    <div class="container bg-light borderForm">
         <div class="row justify-content-center">
-            <h2>Задание №2</h2>
-            <h3>Создать поддомен на testsite</h3>
-            <h3>Создал поддомен с адресом Хоста vrvtask.testsite.co.ua</h3>
+            <div class="col-sm-12 jumbotron text-left bg-light">
+                <h3>Задание №2. Создать поддомен на testsite</h3>
+                <h5>Зайти на ЛК Хостинга и создать поддомен. Уметь пользоватся ЛК.</h5>
+            </div>
+            <div class="col-sm-12 jumbotron text-left">
+            <h3>Создал поддомен с адресом Хоста <a href="http://vrvtask.testsite.co.ua/">vrvtask.testsite.co.ua</a></h3>
+            </div>
         </div>
     </div>
 </section>
--- task5.php
+++ task5.php
@@ -1,11 +1,15 @@
 <?php require "header.php";?>
-    <section class="tasks">
-        <div class="container">
+    <section class="tasks bg-info">
+        <div class="container bg-light borderForm">
             <div class="row justify-content-center">
-                <h2>Задание №5</h2>
-                <h3>Настроить LAMP на локальной машине, запустив внутренний Баш-скрипт для создания сайта</h3>
-                <h3>Создал Баш-скрипт из команд установки LAMP(из сайта faq.testsite)</h3>
-                <h1><a href="bash-script/lampbashvrv" download>Скачать файл</a></h1>
+                <div class="col-sm-12 jumbotron text-left bg-light">
+                    <h3>Задание №5. Настроить LAMP на локальной машине, запустив внутренний Баш-скрипт для создания сайта</h3>
+                    <h5>Выучить и научится пользоватся основными командами Linux из сайта faq.testsite.<br> Сдать экзамен Тим Лиду</h5>
+                </div>
+                <div class="col-sm-12 jumbotron text-left">
+                    <h3>Создал Баш-скрипт из команд установки LAMP<br>(из сайта faq.testsite)</h3>
+                    <a href="bash-script/lampbashvrv" class="btn btn-success" download>Скачать файл</a>
+                </div>
             </div>
         </div>
     </section>
