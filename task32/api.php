4a8033f14dd7e5881f49fb56f1d4325b5f4f0d59
Ruslan Vasilenko <vasilenko.ruslan0321@gmail.com>
2021-01-17 00:26:48 +0200

    fix task32

--- task32/api.php
+++ task32/api.php
@@ -1,4 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Credentials: true");
+header('Content-type: json/application');
+
 require "../init_db.php";
 function getUsers($dbQuery)
 {
