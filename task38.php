756367ef28c3044992acba67d7a3436ea6e5bc87
Ruslan Vasilenko <vasilenko.ruslan0321@gmail.com>
2021-03-16 12:21:01 +0200

    add task38(verstka)

---
+++ task38.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="css/styletask38.css">
+    <title>verstka number two</title>
+</head>
+<body>
+<header>
+    <div class="header">
+        <div class="header__icon">
+            <h2><span>minimø</span></h2>
+        </div>
+        <div class="header_menu">
+            <nav>
+                <ul>
+                    <li><h5><a href="#lifestyle">lifestyle</a></h5></li>
+                    <li><h5><a href="#photodiary">photodiary</a></h5></li>
+                    <li><h5><a href="#music">music</a></h5></li>
+                    <li><h5><a href="#travel">travel</a></h5></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<section>
+    <div class="first-post" id="lifestyle">
+        <div class="first-post__img">
+            <img src="img/imgTask38-1.jpg" alt="weekend">
+        </div>
+        <div class="first-post-content">
+            <h5>photodiary</h5>
+            <h2>The perfect weekend getaway
+            </h2>
+            <input type="checkbox" class="btnLoadMore" id="box_first">
+            <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+                quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+                aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                voluptatem.</p>
+            <label for="box_first" class="btn btn-outline-dark"></label>
+            <h5>Leave a comment</h5>
+        </div>
+    </div>
+    <div class="second-post">
+        <div class="second-post-box">
+            <div class="second-post-box__img">
+                <img src="img/imgTask38-2.jpg" alt="music festival">
+            </div>
+            <div class="second-post-box-content">
+                <h5>Lifestyle</h5>
+                <h2>More than just a music festival </h2>
+                <input type="checkbox" class="btnLoadMore" id="box_1">
+                <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+                    error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+                    qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem.</p>
+                <label for="box_1" class="btn btn-outline-dark"></label>
+            </div>
+        </div>
+        <div class="second-post-box">
+            <div class="second-post-box__img">
+                <img src="img/imgTask38-3.jpg" alt="coffee">
+            </div>
+            <div class="second-post-box-content">
+                <h5>Lifestyle</h5>
+                <h2>Life tastes better with coffee</h2>
+                <input type="checkbox" class="btnLoadMore" id="box_2">
+                <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+                    error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+                    qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem.</p>
+                <label for="box_2" class="btn btn-outline-dark"></label>
+            </div>
+        </div>
+        <div class="second-post-box">
+            <div class="second-post-box__img">
+                <img src="img/imgTask38-4.jpg" alt="dream">
+            </div>
+            <div class="second-post-box-content">
+                <h5>photodiary</h5>
+                <h2>American dream</h2>
+                <input type="checkbox" class="btnLoadMore" id="box_3">
+                <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+                    error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+                    qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem.</p>
+                <label for="box_3" class="btn btn-outline-dark"></label>
+            </div>
+        </div>
+        <div class="second-post-box" id="photodiary">
+            <div class="second-post-box__img">
+                <img src="img/imgTask38-5.jpg" alt="exploring the Alps">
+            </div>
+            <div class="second-post-box-content">
+                <h5>photodiary</h5>
+                <h2>A day exploring the Alps</h2>
+                <input type="checkbox" class="btnLoadMore" id="box_4">
+                <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+                    error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+                    qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem.</p>
+                <label for="box_4" class="btn btn-outline-dark"></label>
+            </div>
+        </div>
+    </div>
+    <div class="newSletter">
+        <div class="newSletter_content">
+            <h2>Sign up for our newsletter!</h2>
+            <form action="">
+                <label for="mail"></label>
+                <input type="text" class="mail" placeholder="Enter a valid email address" id="mail">
+                <button><img src="img/task38-send_icon.png" alt="sendIcon"></button>
+            </form>
+        </div>
+    </div>
+    <div class="second-post">
+        <div class="second-post-box" id="music">
+            <div class="second-post-box__img">
+                <img src="img/imgTask38-6.jpg" alt="running">
+            </div>
+            <div class="second-post-box-content">
+                <h5>lifestyle</h5>
+                <h2>Top 10 song for running</h2>
+                <input type="checkbox" class="btnLoadMore" id="box_5">
+                <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+                    error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+                    qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem.</p>
+                <label for="box_5" class="btn btn-outline-dark"></label>
+            </div>
+        </div>
+        <div class="second-post-box" id="travel">
+            <div class="second-post-box__img">
+                <img src="img/imgTask38-7.jpg" alt="winter road">
+            </div>
+            <div class="second-post-box-content">
+                <h5>lifestyle</h5>
+                <h2>Cold winter days</h2>
+                <input type="checkbox" class="btnLoadMore" id="box_6">
+                <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+                    error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+                    qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem.</p>
+                <label for="box_6" class="btn btn-outline-dark"></label>
+            </div>
+        </div>
+        <div class="btn_loadMore">
+            <h4><button>Load more</button></h4>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="footer">
+        <div class="footer-content">
+            <div class="footer-content_left">
+                <p><a href="#">Terms and conditions</a></p>
+                <p><a href="#">Privacy</a></p>
+            </div>
+            <div class="footer-content_right">
+                <p>Follow</p>
+                <a href=""><span><img src="img/imgTask38-facebook.png" alt="facebook"></span></a>
+                <a href=""><span><img src="img/imgTask38-twitter.png" alt="twitter"></span></a>
+                <a href=""><span><img src="img/imgTask38-instagram.png" alt="instagram"></span></a>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
